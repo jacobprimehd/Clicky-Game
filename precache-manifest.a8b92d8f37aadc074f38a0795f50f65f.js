85f53aa068895f9cb22a316fc7845692fecea242
unknown <zamano.jacob@gmail.com>
2019-08-20 18:17:36 -0700

    Updates

---
+++ precache-manifest.a8b92d8f37aadc074f38a0795f50f65f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56448761bea995d07865f511fe43a718",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "2b1795847f682691db04",
+    "url": "/Clicky-Game/static/css/main.f7cf6c3c.chunk.css"
+  },
+  {
+    "revision": "63b94feeb65724a648fd",
+    "url": "/Clicky-Game/static/js/2.c226a79f.chunk.js"
+  },
+  {
+    "revision": "2b1795847f682691db04",
+    "url": "/Clicky-Game/static/js/main.6e041f19.chunk.js"
+  },
+  {
+    "revision": "86fa00f378e82bf8f1cc",
+    "url": "/Clicky-Game/static/js/runtime~main.0dd9f45f.js"
+  }
+]);
